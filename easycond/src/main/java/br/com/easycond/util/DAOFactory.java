a990ecf86396d9b63b292ff2e78b198cc97a92c4
Weslley Borges <weslleynfs@gmail.com>
2018-05-08 19:56:23 -0300

    Criação do DAO de várias entidades.

--- easycond/src/main/java/br/com/easycond/util/DAOFactory.java
+++ easycond/src/main/java/br/com/easycond/util/DAOFactory.java
@@ -2,9 +2,17 @@ package br.com.easycond.util;
 
 import br.com.easycond.dao.AssembleiaDAO;
 import br.com.easycond.dao.AvisoDAO;
+import br.com.easycond.dao.CondominoDAO;
+import br.com.easycond.dao.FuncionarioDAO;
+import br.com.easycond.dao.PessoaDAO;
+import br.com.easycond.dao.SolicitacaoAvisoDAO;
 import br.com.easycond.dao.UsuarioDAO;
 import br.com.easycond.daointerf.AssembleiaDAOInterf;
 import br.com.easycond.daointerf.AvisoDAOInterf;
+import br.com.easycond.daointerf.CondominoDAOInterf;
+import br.com.easycond.daointerf.FuncionarioDAOInterf;
+import br.com.easycond.daointerf.PessoaDAOInterf;
+import br.com.easycond.daointerf.SolicitacaoAvisoDAOInterf;
 import br.com.easycond.daointerf.UsuarioDAOInterf;
 
 public class DAOFactory {
@@ -14,18 +22,48 @@ public class DAOFactory {
 		usuarioDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
 		return usuarioDAO;
 	}
-	
+
 	public static AvisoDAOInterf criarAvisoDAO() {
 		AvisoDAO avisoDAO = new AvisoDAO();
 		avisoDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
 		return avisoDAO;
 	}
-	
+
 	public static AssembleiaDAOInterf criarAssembleiaDAO() {
 		AssembleiaDAO assembleiaDAO = new AssembleiaDAO();
 		assembleiaDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
 		return assembleiaDAO;
 	}
+
+	public static CondominoDAOInterf criarCondominoDAO() {
+
+		CondominoDAO condominoDAO = new CondominoDAO();
+		condominoDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+
+		return condominoDAO;
+	}
+
+	public static FuncionarioDAOInterf criarFuncionarioDAO() {
+
+		FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+		funcionarioDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+
+		return funcionarioDAO;
+	}
+
+	public static PessoaDAOInterf criarPessoaDAO() {
+
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		pessoaDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+
+		return pessoaDAO;
+	}
 	
-	
+	public static SolicitacaoAvisoDAOInterf criarSolicitacaoAvisoDAO() {
+
+		SolicitacaoAvisoDAO solicitacaoAvisoDAO = new SolicitacaoAvisoDAO();
+		solicitacaoAvisoDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+
+		return solicitacaoAvisoDAO;
+	}
 }
