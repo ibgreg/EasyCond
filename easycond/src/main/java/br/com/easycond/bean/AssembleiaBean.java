4421001872b51331522245204d4dcb6fef00414a
Ítalo Gregório <italo-07@hotmail.com>
2018-05-12 15:31:19 -0300

    Ajustes para o funcionamento do registro de votos da Enquete

--- easycond/src/main/java/br/com/easycond/bean/AssembleiaBean.java
+++ easycond/src/main/java/br/com/easycond/bean/AssembleiaBean.java
@@ -8,7 +8,9 @@ import javax.faces.bean.RequestScoped;
 
 import br.com.easycond.model.Assembleia;
 import br.com.easycond.model.Enquete;
+import br.com.easycond.model.Votos;
 import br.com.easycond.rn.AssembleiaRN;
+import br.com.easycond.rn.VotosRN;
 
 @ManagedBean(name = "assembleiaBean")
 @RequestScoped
@@ -16,8 +18,10 @@ public class AssembleiaBean {
 	
 	private Assembleia assembleia = new Assembleia();
 	private Enquete enquete = new Enquete();
+	private Votos votos = new Votos();
 	
 	private String perguntaEnquete;
+	private String opcaoVoto;
 	private List<Assembleia> lista;
 	
 	@PostConstruct
@@ -30,6 +34,7 @@ public class AssembleiaBean {
 	
 	public String carregarEnquete() {
 		this.assembleia = new Assembleia();
+		this.votos = new Votos();
 		AssembleiaRN assembleiaRN = new AssembleiaRN();
 		assembleia = assembleiaRN.carregarAssembleia();
 		return "/restrito/assembleia/votacao";
@@ -49,6 +54,18 @@ public class AssembleiaBean {
 		assembleiaRN.salvar(this.assembleia);
 		
 		return "/adm/assembleia/lista";
+	}
+	
+	public String enviarVoto() {
+		AssembleiaRN assembleiaRN = new AssembleiaRN();
+		assembleia = assembleiaRN.carregarAssembleia();
+		votos.setIdEnquete(assembleia.getEnquete());
+		
+		VotosRN votosRN = new VotosRN();
+		votosRN.salvar(this.votos);
+		
+		return "/restrito/assembleia/resultado";
+		
 	}
 	
 	public String editar() {
@@ -78,12 +95,28 @@ public class AssembleiaBean {
 		this.enquete = enquete;
 	}
 	
+	public Votos getVotos() {
+		return votos;
+	}
+
+	public void setVotos(Votos votos) {
+		this.votos = votos;
+	}
+
 	public String getPerguntaEnquete() {
 		return perguntaEnquete;
 	}
 
 	public void setPerguntaEnquete(String perguntaEnquete) {
 		this.perguntaEnquete = perguntaEnquete;
+	}
+	
+	public String getOpcaoVoto() {
+		return opcaoVoto;
+	}
+
+	public void setOpcaoVoto(String opcaoVoto) {
+		this.opcaoVoto = opcaoVoto;
 	}
 
 	public List<Assembleia> getLista() {
