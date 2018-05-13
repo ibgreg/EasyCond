de4cbaca14daf0fbbf25e4e7184dea5b5ff6dddd
Weslley Borges <weslleynfs@gmail.com>
2018-05-13 19:39:03 -0300

    Importação de java.util.Set, geração dos métodos hashCode e equals e definição do nome da coluna que contém o id do apartamento.

--- easycond/src/main/java/br/com/easycond/model/Apartamento.java
+++ easycond/src/main/java/br/com/easycond/model/Apartamento.java
@@ -1,11 +1,13 @@
 package br.com.easycond.model;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,11 +18,15 @@ public class Apartamento implements Serializable{
 
 	@Id
 	@GeneratedValue
+	@Column(name="id_apartamento")
 	private Integer id;
 	
 	@Column(name = "num_apartamento", nullable = false)
 	private Integer numApartamento;
 
+	@ManyToMany(mappedBy="apartamento")
+	private Set<Bloco> bloco;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -28,5 +34,59 @@ public class Apartamento implements Serializable{
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+	public Integer getNumApartamento() {
+		return numApartamento;
+	}
+
+	public void setNumApartamento(Integer numApartamento) {
+		this.numApartamento = numApartamento;
+	}
+
+	public Set<Bloco> getBloco() {
+		return bloco;
+	}
+
+	public void setBloco(Set<Bloco> bloco) {
+		this.bloco = bloco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bloco == null) ? 0 : bloco.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((numApartamento == null) ? 0 : numApartamento.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Apartamento other = (Apartamento) obj;
+		if (bloco == null) {
+			if (other.bloco != null)
+				return false;
+		} else if (!bloco.equals(other.bloco))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (numApartamento == null) {
+			if (other.numApartamento != null)
+				return false;
+		} else if (!numApartamento.equals(other.numApartamento))
+			return false;
+		return true;
+	}	
+	
 	
 }
