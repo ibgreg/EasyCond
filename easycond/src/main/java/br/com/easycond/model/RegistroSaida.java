bbfb81fc4619d791d8dc5a07f37a4ecf62a50f28
Weslley Borges <weslleynfs@gmail.com>
2018-05-20 01:29:59 -0300

    Implementação dos métodos hashCode e equals.

--- easycond/src/main/java/br/com/easycond/model/RegistroSaida.java
+++ easycond/src/main/java/br/com/easycond/model/RegistroSaida.java
@@ -56,6 +56,41 @@ public class RegistroSaida implements Serializable{
 	public void setDataSaida(Date dataSaida) {
 		this.dataSaida = dataSaida;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataSaida == null) ? 0 : dataSaida.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((idCondomino == null) ? 0 : idCondomino.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RegistroSaida other = (RegistroSaida) obj;
+		if (dataSaida == null) {
+			if (other.dataSaida != null)
+				return false;
+		} else if (!dataSaida.equals(other.dataSaida))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (idCondomino == null) {
+			if (other.idCondomino != null)
+				return false;
+		} else if (!idCondomino.equals(other.idCondomino))
+			return false;
+		return true;
+	}
 }
--- easycond/src/main/java/br/com/easycond/model/RegistroVisita.java
+++ easycond/src/main/java/br/com/easycond/model/RegistroVisita.java
@@ -57,5 +57,48 @@ public class RegistroVisita implements Serializable {
 
 	public void setDataFimVisita(Date dataFimVisita) {
 		this.dataFimVisita = dataFimVisita;
-	}	
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataFimVisita == null) ? 0 : dataFimVisita.hashCode());
+		result = prime * result + ((dataInicioVisita == null) ? 0 : dataInicioVisita.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((idVisitante == null) ? 0 : idVisitante.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RegistroVisita other = (RegistroVisita) obj;
+		if (dataFimVisita == null) {
+			if (other.dataFimVisita != null)
+				return false;
+		} else if (!dataFimVisita.equals(other.dataFimVisita))
+			return false;
+		if (dataInicioVisita == null) {
+			if (other.dataInicioVisita != null)
+				return false;
+		} else if (!dataInicioVisita.equals(other.dataInicioVisita))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (idVisitante == null) {
+			if (other.idVisitante != null)
+				return false;
+		} else if (!idVisitante.equals(other.idVisitante))
+			return false;
+		return true;
+	}		
 }
