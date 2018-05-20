5dda64eb50c5d6f356b7781ef49ba3f72e952481
Weslley Borges <weslleynfs@gmail.com>
2018-05-20 00:03:03 -0300

    Relação com a entidade Pessoa e implementação do hashCode e equals.

--- easycond/src/main/java/br/com/easycond/model/RegistroEntrada.java
+++ easycond/src/main/java/br/com/easycond/model/RegistroEntrada.java
@@ -6,14 +6,24 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.hibernate.annotations.ForeignKey;
+
 @Entity
 @Table(name="ingresso_condomino")
 public class RegistroEntrada implements Serializable{
+
+	public RegistroEntrada() {
+		super();
+		this.pessoa = new Pessoa();
+	}
 
 	/**
 	 * 
@@ -21,11 +31,15 @@ public class RegistroEntrada implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="id_ingresso")
 	private Integer id;
 	
-	private Integer idCondomino;
+	@SuppressWarnings("deprecation")
+	@ManyToOne
+	@JoinColumn(name="pessoa", nullable=false)
+	@ForeignKey(name="id_condomino")
+	private Pessoa pessoa;
 	
 	//private Integer idImovel;
 	
@@ -39,14 +53,14 @@ public class RegistroEntrada implements Serializable{
 
 	public void setId(Integer id) {
 		this.id = id;
+	}	
+
+	public Pessoa getPessoa() {
+		return pessoa;
 	}
 
-	public Integer getIdCondomino() {
-		return idCondomino;
-	}
-
-	public void setIdCondomino(Integer idCondomino) {
-		this.idCondomino = idCondomino;
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
 	}
 
 	public Date getDataEntrada() {
@@ -55,6 +69,43 @@ public class RegistroEntrada implements Serializable{
 
 	public void setDataEntrada(Date dataEntrada) {
 		this.dataEntrada = dataEntrada;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataEntrada == null) ? 0 : dataEntrada.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((pessoa == null) ? 0 : pessoa.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RegistroEntrada other = (RegistroEntrada) obj;
+		if (dataEntrada == null) {
+			if (other.dataEntrada != null)
+				return false;
+		} else if (!dataEntrada.equals(other.dataEntrada))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (pessoa == null) {
+			if (other.pessoa != null)
+				return false;
+		} else if (!pessoa.equals(other.pessoa))
+			return false;
+		return true;
 	}
 	
 	
