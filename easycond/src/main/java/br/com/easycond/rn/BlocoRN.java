8164f8746fa2e65cb28c10a6828d60f11da6e64f
Weslley Borges <weslleynfs@gmail.com>
2018-05-27 01:23:31 -0300

    Relação entre entidades.

--- easycond/src/main/java/br/com/easycond/rn/BlocoRN.java
+++ easycond/src/main/java/br/com/easycond/rn/BlocoRN.java
@@ -2,6 +2,7 @@ package br.com.easycond.rn;
 
 import java.util.List;
 
+import br.com.easycond.dao.ApartamentoDAO;
 import br.com.easycond.daointerf.BlocoDAOInterf;
 import br.com.easycond.model.Bloco;
 import br.com.easycond.util.DAOFactory;
@@ -27,6 +28,8 @@ public class BlocoRN {
 
 	public void salvar(Bloco bloco) {
 
+		bloco.setApartamento(new ApartamentoRN().listar());
+		
 		Integer id = bloco.getId();
 
 		if (id == null || id == 0) {
